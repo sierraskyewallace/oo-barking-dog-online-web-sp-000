aec2fd748f993550845c18c4ee5c74796dc7ae9c
Sierra Wallace <sskwallace@gmail.com>
2020-08-27 23:42:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,12 @@
 class Dog 
   def name=(dog_name)
-    the_dog_name = dog_name
+    @@the_dog_name = dog_name
   end
   def bark 
-    puts "Woof!"
+    @@puts "Woof!"
   end
 end
 
 fido = Dog.new 
-fido.name 
-fido.bark
+puts fido.name 
+puts fido.bark
